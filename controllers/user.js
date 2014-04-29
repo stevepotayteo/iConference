38f4d06354ebae79d5dcc36ac6b0f9ef9b4d9ecf
Steve Teo <xylancer@gmail.com>
2014-04-29 08:23:34 +0800

    Early socket example

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,22 @@
 module.exports.controller = function(app) {
   app.get('/', function(req, res) {
-    res.render('index')
+    res.render('index');
   });
 
   app.get('/signup', function(req, res) {
-    res.render('index')
+    res.render('index');
   });
 
   app.get('/login', function(req, res) {
-    res.render('index')
+    res.render('index');
   });
-}
+
+  var sio = app.get('sio');
+
+  sio.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+};
